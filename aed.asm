92335b47f49df8878a1f428fece8b0b55b9111c0
DragonShip20 <wardzinski.antoni@gmail.com>
2026-05-29 17:27:00 +0200

    aed: implemented getting char from stdin

--- aed.asm
+++ aed.asm
@@ -1,13 +1,19 @@
 section .data
-output db "Hello world", 10
-output_len equ $-output
+buffer times 512 db 0
+cursor dq 0
 
 section .text
 global _start
 
-_start:
-    mov rax, output
-    mov rbx, output_len
+_start: 
+    mov rax, buffer
+    add rax, [cursor]
+    call getc
+    
+    inc qword [cursor]
+
+    mov rax, buffer
+    mov rbx, 10
     call print
 
     call exit
@@ -24,5 +30,14 @@ print:
     mov rdi, 1
     pop rsi
     pop rdx
+    syscall
+    ret
+
+getc:
+    push rax
+    mov rax, 0
+    mov rdi, 0
+    pop rsi
+    mov rdx, 1
     syscall
     ret
