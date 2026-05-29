e304c753989af4b645a9ff22cb43e5cc02b0140b
DragonShip20 <wardzinski.antoni@gmail.com>
2026-05-29 17:58:01 +0200

    aed: replace small (one syscall) functions with macros

--- aed.asm
+++ aed.asm
@@ -1,3 +1,25 @@
+%macro exit 1
+    mov rax, 60
+    mov rdi, %1
+    syscall
+%endmacro
+
+%macro print 2
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, %1
+    mov rdx, %2
+    syscall
+%endmacro
+
+%macro getc 1
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, %1
+    mov rdx, 1
+    syscall
+%endmacro
+
 section .data
 ;; Our buffer is currently 512b, will be expanded
 buffer times 512 db 0
@@ -8,39 +30,4 @@ global _start
 
 _start: 
     ;; TODO: set raw terminal mode here
-    mov rax, buffer
-    add rax, [cursor]
-    call getc
-    
-    inc qword [cursor]
-
-    mov rax, buffer
-    mov rbx, 10
-    call print
-
-    call exit
-
-;; TODO: replace small functions (one syscall) with macros
-exit:
-    mov rax, 60
-    xor rdi, rdi
-    syscall
-
-print:
-    push rbx
-    push rax
-    mov rax, 1
-    mov rdi, 1
-    pop rsi
-    pop rdx
-    syscall
-    ret
-
-getc:
-    push rax
-    mov rax, 0
-    mov rdi, 0
-    pop rsi
-    mov rdx, 1
-    syscall
-    ret
+    exit 0
