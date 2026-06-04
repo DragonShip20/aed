d42fd88cf0493b936f760ed3526192f35f19fca4
DragonShip20 <wardzinski.antoni@gmail.com>
2026-06-04 20:21:49 +0200

    aed: add backspace handling

--- aed.asm
+++ aed.asm
@@ -7,6 +7,8 @@ section .data
 ;; Our buffer is currently 512b, will be expanded
 buffer times 512 db 0
 cursor dq 0
+length dq 0
+input db 0
 
 ;; Constants for setting terminal raw mode
 TCGETS equ 0x5401
@@ -20,8 +22,10 @@ ICRNL  equ 0000400h
 clear_screen db 27,"[2J",27,"[H"
 clear_screen_len equ $ - clear_screen
 
-;; Keyboard shortcuts
+;; Special keys
 CTRL_Q equ 17
+BACKSPACE1 equ 127
+BACKSPACE2 equ 8
 
 section .text
 global _start
@@ -31,17 +35,53 @@ _start:
     call render
 
 .main_loop:
-    mov rbx, [cursor]
-    lea r8, [buffer+rbx]
+    mov r8, input
     getc r8
+    call handle_char
+    mov al, [r8]
+    cmp al, 0x20
+    jb .loop_finish
+    cmp al, 0x7E
+    ja .loop_finish
+
+    mov rbx, [cursor]
+    mov [buffer + rbx], al
+
+.loop_finish:
     inc qword [cursor]
-    cmp byte [r8], CTRL_Q
-    je .main_loop_end
+    inc qword [length]
     call render
     jmp .main_loop
     
 .main_loop_end:
     exit 0
+
+handle_char:
+    cmp byte [r8], CTRL_Q
+    je _start.main_loop_end
+    cmp byte [r8], BACKSPACE1
+    je handle_backspace
+    cmp byte [r8], BACKSPACE2
+    je handle_backspace
+    ret
+
+handle_backspace:
+    cmp qword [cursor], 0
+    je .done
+    dec qword [cursor]
+    dec qword [length]
+    mov rbx, [cursor]
+
+.shift_loop:
+    mov al, [buffer+rbx+1]
+    mov [buffer+rbx], al
+    inc rbx
+    cmp rbx, [length]
+    jbe .shift_loop
+
+.done:
+    call render
+    jmp _start.main_loop
 
 render:
     ;; TODO: optimise this, make this faster
